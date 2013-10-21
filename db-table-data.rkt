a21361dd065d5e69cf09b48c91b98ef54dee9701
Arno De Witte <Arno.De.Witte@vub.ac.be>
2013-10-21 15:38:07 +0200

    implementing phase 1

--- db-table-data.rkt
+++ db-table-data.rkt
@@ -1,1 +1,26 @@
 #lang racket
+
+(define db-table-data%
+  (class object%
+    (init list-of-vectors) 
+    (define* 
+      [data~ list-of-vectors]
+      [index~ 0]
+      [amount-of-rows~ 'not-counted]) ;stored for performance
+    
+    (super-new)
+    
+    (define/public (get-next-row)
+      (let ([ret  (list-ref data~ index~)])
+        (set! index~ (+ index~ 1))
+        ret))
+    
+    (define/public (number-rows)
+      (if (eq? amount-of-rows~ 'not-counted)
+          (begin
+            (set! amount-of-rows~ (length data~))
+            amount-of-rows~)
+          amount-of-rows~))
+    
+    )
+  )
