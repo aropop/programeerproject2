8e44ae91fbf35db8d6a8114503ddc58230b22c15
Arno De Witte <Arno.De.Witte@vub.ac.be>
2013-10-23 14:23:25 +0200

    Added macro

---
+++ macros.rkt
@@ -0,0 +1,10 @@
+#lang racket
+
+(provide define*)
+(define-syntax define*
+  (syntax-rules ()
+    [(define* [a b]) (define a b)]
+    [(define* (a b) (c d) ...) (begin
+                       (define a b)
+                       (define* (c d) ...))]))
+
